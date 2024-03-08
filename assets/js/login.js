97b0e85bed3db7b53ff26777e8b289a477b867d5
Efrain Daza <dazaa94@hotmail.com>
2024-03-08 03:24:41 -0300

    agregado ventan de login falta terminar

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+
+// Función para abrir la ventana modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+// Función para cerrar la ventana modal
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
