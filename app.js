c9d37869e1b2851f8b85ed3d86db5613178775c5
Efrain Daza <dazaa94@hotmail.com>
2024-03-16 11:51:28 -0300

    se creo archivo gitignore, sumando que hay que hacer un ruteo nuevos de algunos archivos y recursos

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const path = require("path")//busca rutas mas rapidas y las muestra en el navegador
-
 //solictamos el expres que anteriormente guardamos en un variable
 const app = express();
+app.use(express.static("public"));
+
+
+
 
 //poner a correr el servidor
-app.listen(3000, () => {
-    console.log("es exitoso la marcha")
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("servidor corriendo en el puerto 3000")
 })
 //comprobacion de que funciona el servidor-rutas
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./views/index.html"))
+    res.sendFile(path.join(__dirname, "/views/index.html"))
 })
