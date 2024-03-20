0a6462ccfc1926a139d412f4fdf0093798cb20d6
Efrain Daza <dazaa94@hotmail.com>
2024-03-20 04:20:27 -0300

    corregir vista login

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const path = require("path")//busca rutas mas rapidas y las muestra en el navegador
+const path = require("path");//busca rutas mas rapidas y las muestra en el navegador
 //solictamos el expres que anteriormente guardamos en un variable
 const app = express();
 app.use(express.static("public"));
-
-
 
 
 //poner a correr el servidor
@@ -14,4 +12,14 @@ app.listen(process.env.PORT || 3000,()=>{
 //comprobacion de que funciona el servidor-rutas
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+app.get("/products", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views/products.html"))
+})
+app.get("/register", function (req,res){
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/login", function (req,res){
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
